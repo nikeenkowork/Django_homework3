7e9b5f70700fe804a6751d0ab2e676f9feb99929
Denys <nikeenko89@gmail.com>
2026-06-14 18:40:27 +0200

    Add initial migrations for Category and Product models

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "catalog"
+    "catalogy"
 
 ]
 
@@ -74,14 +74,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "screencast",
-        "USER": "Denys",
-        "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "5432"
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
